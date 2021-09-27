798ac4ee49054ae013ed1926ca571f3e90c85489
michealgarcia <garciamicheal0@outlook.com>
2021-09-26 19:55:53 -0700

    Starting on a game

---
+++ py3hardway/ex27.py
@@ -0,0 +1,25 @@
+"""We need to print out flash cards to memorize logic
+
+Truth tables"""
+
+"""
+NOT
+not False == True
+not True == False
+
+OR
+True or False == True
+True or True == True
+False or True == True
+False or False == False
+
+AND
+True and False == False
+True and True == True
+False and True == False
+False and False == False
+
+NOT OR
+not (True or False) == False
+not (True or True) == False
+"""
---
+++ py3hardway/first_game.py
@@ -0,0 +1,20 @@
+print("""\n\n\t\t\tWelcome to my game!
+
+\t   This is a game that will always end the same.
+\t       But there is only one way to beat it.
+
+
+\t\t  Life is about the journey and I 
+\t      hope you enjoy yours in this wonderful 
+\t\t\t little adventure.
+
+"And then the day came,
+when the risk
+to remain tight
+in a bud
+was more painful
+than the risk
+it took
+to blossom."
+\t--Risk, by Anaïs Nin
+""")
