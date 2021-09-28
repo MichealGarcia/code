3ae709d895db1ce701c5b02634445d883f6b3fe6
michealgarcia <garciamicheal0@outlook.com>
2021-09-27 18:23:02 -0700

    still working on game

--- py3hardway/first_game.py
+++ py3hardway/first_game.py
@@ -1,3 +1,28 @@
+"""
+Location: 
+    Continent = "North America"
+    City = "One large city in the distance!"
+    Lifeforms = {"humans": {"Random Guy", "you": "You"}, 
+                "Creatures": 
+                    {"Little Pixies": 
+                        {"names": 
+                            {"Jeff": {"height": "tallest", "weight": "thin", "temper": "timid},
+                            {"Sonny": {"height": "shortest", "weight": "Is that a lil' pumkin?!", "temper": "sweet like candy"},
+                            {"Sam": {"height": "In between the other two", "weight": "What i expected in a fairy.", "temper": "helpful"}
+                    {"Animals":
+                        {"species": 
+                            ("Bear", "Some dog looking thing", 
+                            "Is that a mushroom?", "Mimic", "Squirrel", 
+                            "Something", "MissingNo", "Mark Zuckerberg")}    
+                        }}}}
+    landmarks = "city in distance"
+    borders = "There is a large wall around me"
+    landscape = ("Trees", "hills", "lake", "cave")
+    magic = "
+    """
+
+
+
 print("""\n\n\t\t\tWelcome to my game!
 
 \t   This is a game that will always end the same.
@@ -18,3 +43,4 @@ it took
 to blossom."
 \t--Risk, by Anaïs Nin
 """)
+
