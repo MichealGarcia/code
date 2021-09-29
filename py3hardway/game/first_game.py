96f6e922aad9b1eeb5d22c7f7e70164e3c8bc245
michealgarcia <garciamicheal0@outlook.com>
2021-09-28 20:35:27 -0700

    Made separate game folder

---
+++ py3hardway/game/first_game.py
@@ -0,0 +1,46 @@
+"""
+Location: 
+    Continent = "North America"
+    City = "One large city in the distance!"
+    Lifeforms = {"humans": {"Random Guy", "you": "You"}, 
+                "Creatures": 
+                    {"Little Pixies": 
+                        {"names": 
+                            {"Jeff": {"height": "tallest", "weight": "thin", "temper": "timid},
+                            {"Sonny": {"height": "shortest", "weight": "Is that a lil' pumkin?!", "temper": "sweet like candy"},
+                            {"Sam": {"height": "In between the other two", "weight": "What i expected in a fairy.", "temper": "helpful"}
+                    {"Animals":
+                        {"species": 
+                            ("Bear", "Some dog looking thing", 
+                            "Is that a mushroom?", "Mimic", "Squirrel", 
+                            "Something", "MissingNo", "Mark Zuckerberg")}    
+                        }}}}
+    landmarks = "city in distance"
+    borders = "There is a large wall around me"
+    landscape = ("Trees", "hills", "lake", "cave")
+    magic = "
+    """
+
+
+
+print("""\n\n\t\t\tWelcome to my game!
+
+\t   This is a game that will always end the same.
+\t       But there is only one way to beat it.
+
+
+\t\t  Life is about the journey and I 
+\t      hope you enjoy yours in this wonderful 
+\t\t\t little adventure.
+
+"And then the day came,
+when the risk
+to remain tight
+in a bud
+was more painful
+than the risk
+it took
+to blossom."
+\t--Risk, by Anaïs Nin
+""")
+
