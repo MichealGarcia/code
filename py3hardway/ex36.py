b74b9ae16c614fe3d946b7863f1d1509e60a507d
michealgarcia <garciamicheal0@outlook.com>
2021-10-07 18:03:09 -0700

    updating progress

---
+++ py3hardway/ex36.py
@@ -0,0 +1,29 @@
+#Rules for for-loops and while-loops.
+#Tips for debugging
+#Another game
+
+
+#Every if-statement must have an else
+#if else wouldn't run, then youwill find errors
+
+#never nest if-statements more than two deep, always try to stay one deep
+
+#Treat if-statements like paragrapghs, put blank lines before and after
+
+#Boolean tests should be simple
+
+
+#While-loops
+#Rarely use these (they are to loop forever)
+
+#Use for-loop for everything else
+
+#DEBUGGING
+
+#Don't use a debugger
+#Use print to print out the balues of variables at different points in the program
+
+#Run code before moving to the next block
+
+
+#You have already made a small game, so lets move on.
