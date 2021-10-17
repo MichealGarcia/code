1df08a321064b3eb216729bc1fc91f4f887b16a9
michealgarcia <garciamicheal0@outlook.com>
2021-10-17 16:24:15 -0700

    Started game premise

---
+++ py3hardway/game/gamev3.py
@@ -0,0 +1,83 @@
+# # # ### Making a game
+
+# # # # Open-world in a desert, there is a town nearby, an abandoned castle that 
+# # # # is known to be haunted. The town-folk warn adventurers to stay clear of 
+# # # # this mansion. The mansion was originally a center to a very old village
+# # # # lost to time. 
+
+# # # # In this world magic and supernatural is known generally, and explored
+# # # # by adventurers, you are one of these adventurers, known as hunters
+# # # # These hunters are mixed between several races (minotaurs, human, mythical beings)
+
+# # # # hunters* are renwoned for hunting Djinn(evil spirits), and eliminating
+# # # # them. When the hunter kills a Djinn it is possible to take on the powers
+# # # # of the spirit. 
+# # # #     There are 3 main types of enemy Djinns. 
+# # # #         Blue, weakest huntable spirits
+# # # #         red, mid-tier, need experience and strength
+# # # #         purple, strongest, op as fuck, need smarts/abilities along power.
+# # # #
+# # # #
+# # # #    BLUE: If you killed Djinn that had an ability better than yours, you could
+# # #       take on ONE of those abilities or buffing attributes.
+# #        RED: You can take on a physical attribute of these (If a minotaur, horns, or strength)
+# #        PURPLE: These are unknown, as very little have been defeated.
+            
+
+# # import time
+# # import termcolor
+# # import os
+
+# from ast import walk
+# from game.gamev2 import start
+
+
+# Location
+#         Town
+#         Haunted-Town
+#             upper-town
+#             lower-town
+
+# structures
+#     house
+#     shopping district
+#     church/shrine
+#     town hall
+#     guild
+#     town well
+#     town garden
+#     apothecary
+#     library
+#     carriage
+#     ruins
+#     Mansion
+
+    
+# Creature
+#     Minotaur
+#     Djinn
+#     Human
+#     half-Elf
+#     lizard-folk
+#     tabaxi(cat thing)
+#     Dwarf
+
+
+# You must travel through the haunted village, and defeat Djinn
+# as they continue to devestate the nearby town and its people.
+
+# Map
+#     -travel, This is when the player goes to a carriage and travels to a location
+
+# Engine
+#     - start
+
+# Location
+#     - enter
+#     - walk
+#     - travel by carriage
+#     - 
+
+
+
+
