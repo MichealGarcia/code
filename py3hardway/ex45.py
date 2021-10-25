b0acbe79c45fdc75e88528bb5d88a7b6f261a960
michealgarcia <garciamicheal0@outlook.com>
2021-10-25 12:22:19 -0700

    ex45 make game.

---
+++ py3hardway/ex45.py
@@ -0,0 +1,28 @@
+# This will run my game. I will create a file for characters, rooms, treasures.
+
+# The game:
+    # You start out on a hill tall enough to see everything at a distance
+    # To the North is forest
+        # Forest has 2 locations
+            # Left Path, small pond with fairies
+                #Touch water, get amulet, return to hill
+            # Right path, bear den
+    # To the West is a lake
+        # The lake has 2 locations
+            # bank, there is a small boat and a talking mask
+            # small island, On the island is a puppet with latches on its head (for mask)\
+                #Put mask on puppet, transport to hill, gain mask
+    # To the East is a small desert
+        # The desert has one location
+            # In the distance as you start to head towards the desert you see
+            # a giant pool of water
+            # Location, you suddenly appear in an Oasis after passing out.
+                # Oasis, a skeleton next to a sword
+                    #skeleton talks
+    # To the South is a little house and a farm
+        # The Farm has a barn, a cottage, and a fenced area with sheep
+            # barn, has one Hay bail in the middle (holds key)
+            # cottage, smells like cooking (can't go inside without key)
+                # Once inside, there is nothing but cobwebs and a fork on the table
+                    #Grab Fork, and transport to hill
+            # fenced area, sheep that just bah!
