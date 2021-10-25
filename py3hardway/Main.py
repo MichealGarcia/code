3478266cad6dbefd15e3077e9c4305246389e269
michealgarcia <garciamicheal0@outlook.com>
2021-10-25 11:55:43 -0700

    Updating code, moving back to exercises

--- py3hardway/Main.py
+++ py3hardway/Main.py
@@ -18,37 +18,37 @@
 
 import sys
 
-class Meeting(object):
+# class Meeting(object):
 
-    def __init__(self):
-        self.handshake = True
-        self.hello = True
-        self.ignore = False
-        self.possible_interaction = True
+#     def __init__(self):
+#         self.handshake = True
+#         self.hello = True
+#         self.ignore = False
+#         self.possible_interaction = True
 
-    def shake_hand(self, handshake):
-        if handshake == True:
-            self.shake_hand_intro()
-        else:
-            handshake = True
-            self.shake_hand(handshake)
+#     def shake_hand(self, handshake):
+#         if handshake == True:
+#             self.shake_hand_intro()
+#         else:
+#             handshake = True
+#             self.shake_hand(handshake)
 
-    def say_hello(self, hello):
-        if hello == True:
-            self.hello_intro()
-        else:
-            hello = True
-            self.say_hello(hello)
+#     def say_hello(self, hello):
+#         if hello == True:
+#             self.hello_intro()
+#         else:
+#             hello = True
+#             self.say_hello(hello)
     
-    def ignore_stranger(self, ignore):
-        if ignore == True:
-            self.no_interaction()
-        else:
-            ignore = True
-            self.ignore_stranger(ignore)
+#     def ignore_stranger(self, ignore):
+#         if ignore == True:
+#             self.no_interaction()
+#         else:
+#             ignore = True
+#             self.ignore_stranger(ignore)
     
-    def shake_hand_intro(self):
-        print("**You shake hands and introduce yourselves.**")
+#     def shake_hand_intro(self):
+#         print("**You shake hands and introduce yourselves.**")
 
 
 class Human(object):
@@ -60,49 +60,79 @@ class Human(object):
         self.job = ""
         self.age = ""
         self.description = []
+        self.introduced = False
 
     def persona(self):
-        d = self.description
-        self.name = d.append(input("What's your name?:\n"))
-        self.job = d.append(input("What do you do?:\n"))
-        self.age = d.append(input("How old are you?:\n"))
+        describe = self.description
+        describe.append(input("Name?:\n"))
+        describe.append(input("Job?:\n"))
+        describe.append(input("Age?:\n"))
+        self.name = describe[0]
+        self.job = describe[1]
+        self.age = describe[2]
+
+    def existence(self):
+        if self.exists == True:
+            self.persona()
+        else:
+            print("It appears you don't exist, goodbye.")
+            sys.exit()
+    
+    def introduction(self):
+
+
+
+    def friend(self, introduction):
+        if 
+
+            
+    
+
 
 
 class You(Human):
     pass
 
 class Stranger(Human):
-
-    def __init__(self):
-        self.seems_rude = False
-    pass
-
-class Friend(Human):
-
-    def __init__(self):
-        self.introduced = True
     pass
     
 
 
 you = You()
-
+jeff = Stranger()
+jeff.exists = True
 #print attribute to see if you exist. Debug
 #print(you.exists)you.exists = False
 
 #Prove you exist by describing yourself
-if you.exists == True:
-
-    you.persona()
-
-else:
-    print("It appears you don't exist, goodbye.")
-    sys.exit()
-
+you.existence()
+jeff.existence()
+friendship1 = you.friends(you, jeff)
 print(you.description)
+print(you.name)
+print(you.age)
+print(you.job)
+print(jeff.age)
+
+print(friendship1)
 
 
 
 
 
 
+# Stopping coding, reviewing current work
+
+# I think I will restart, and create humans as my main object. 
+
+# Class Human
+    # has-a __init__ attributes: name, job, age
+    # has-a introduction
+    # has-a persona
+    # has-a existence
+
+# You is-a Human
+# Stranger is-a Human
+    # has-a not_introduced
+# Friend is-a Human
+    # has-a introduced
